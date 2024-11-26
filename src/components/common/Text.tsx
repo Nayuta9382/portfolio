1e7f9f0102b196e2c8ec50857255110a49c772bb
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2024-11-26 15:26:32 +0900

    テキストコンポーネントの作成 #2

---
+++ src/components/common/Text.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+type Props = {
+    lavel?: 'main' | 'heading';   // sizeプロパティはsmall, medium, largeのいずれか                      // colorプロパティは任意の文字列（色）
+    children: React.ReactNode;             // childrenはReactの任意のコンテンツ
+  };
+const Text :React.FC<Props> = ({lavel = 'main', children}) => {
+    // フォントサイズの定義(デフォルトはメインテキスト)
+    let fontSize:string = '';
+    let  weight:string = '';
+    let  color:string = '';
+    switch (lavel) {
+        case 'main':
+          fontSize = 'text-base'
+          weight = 'font-normal';
+          color = 'font-black';
+          break;
+        case 'heading':
+            fontSize = 'text-lg'// 'PC'の場合のフォントサイズ
+            weight = 'font-bold';
+            color = 'font-black';
+          break;
+      }
+    
+
+    return (
+        <p className={`${fontSize}  ${weight} ${color}`}>{children}</p>
+    );
+}
+ 
+export default Text;
