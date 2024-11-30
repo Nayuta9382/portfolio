f6910e80178d3088428f88669c8fe09d5b9865fd
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2024-11-30 22:09:50 +0900

    Tabelのmin-widthを計算する関数の作成 #4

---
+++ src/hooks/useTextWidth.ts
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+
+const UseTextWidth = (text: string, lineSize:1|2) => {
+  const [windowWidth, setWindowWidth] = useState<number>(0); // 初期値として0
+  const [textSize, setTextSize] = useState<number>(0);
+
+  useEffect(() => {
+    // クライアントサイドでのみ実行
+    if (typeof window !== "undefined") {
+      // 初回実行: windowWidth の設定
+
+        setWindowWidth(window.innerWidth); // 初期の画面幅を取得
+
+
+      // 画面幅が変更された時に実行される処理
+      const clacWidth = () => {
+        // 必要項目一覧
+        const fontSize = '16px';
+        const fontFamily = 'Noto Serif JP, serif';
+        // const fontOpticalSizing = 'auto'; // フォントの光学サイズ
+
+        // Canvas要素を作成
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        let textWidth = 0;
+
+        if (context) {
+          // フォントを設定
+          context.font = `${fontSize} ${fontFamily}`;
+          // テキストの横幅を計測
+          textWidth = context.measureText(text).width;
+        } else {
+          // nullの場合のみ簡易的に横幅を計算
+          textWidth = text.length * 16;
+        }
+
+        // 文字サイズ以外の必要な幅の計算(padding) 
+        const paddingWidth = (windowWidth >= 768 ? 32 : 16);
+        console.log(textWidth);
+        // console.log(paddingWidth);
+        
+        const result = textWidth / lineSize  + paddingWidth + 10; // 10は微調整
+        if (result !== textSize) {
+          setTextSize(result); // textSizeを更新
+        }
+      };
+
+      // 初回に実行
+      clacWidth();
+
+      // リサイズイベントリスナーを追加
+      const resizeListener = () => {
+        setWindowWidth(window.innerWidth); // 画面幅を更新
+      };
+
+      window.addEventListener('resize', resizeListener);
+
+      // クリーンアップ関数：アンマウント時にリスナーを削除
+      return () => {
+        window.removeEventListener('resize', resizeListener);
+      };
+    }
+  }, [windowWidth]); // `windowWidth` 
+
+
+  return textSize;
+};
+
+export default UseTextWidth;
