4633bfeb1fc2fd32b915c826dd4197ff3fc826b5
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2024-12-29 23:08:41 +0900

    topに戻るボタンを作成 #12

--- src/components/layout/SidebarLayout.tsx
+++ src/components/layout/SidebarLayout.tsx
@@ -1,7 +1,8 @@
-import { FC, useState } from "react";
+import { FC,  useState } from "react";
 import NavButton from "../ui/navMenu/NavButton";
 import NavMenuList from "../ui/navMenu/NavMenuList";
 import { navMenu } from "@/types/navMenu";
+import ScrollTopButton from "../ui/navMenu/ScrollTopButton";
 
 interface SidebarLayoutProps {
     titles:navMenu[];
@@ -11,10 +12,10 @@ interface SidebarLayoutProps {
 const SidebarLayout: FC<SidebarLayoutProps> = ({titles,navInfo}) => {
     // Navメニューが閉じているか開いているかを格納するState
     const [openNavFlg, setOpenNavFlg] = useState(false);
-
     return (
-        <div className="fixed top-0 right-0 z-50">
+        <div className="fixed w-[75px] h-[100vh] top-0 right-0 z-50">
             <NavButton openNavFlg={openNavFlg} setOpenNavFlg={setOpenNavFlg} />
+            <ScrollTopButton/>
             <NavMenuList titles={titles} navInfo={navInfo} openNavFlg={openNavFlg} setOpenNavFlg={setOpenNavFlg}/>
         </div>
     );
