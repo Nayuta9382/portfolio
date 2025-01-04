e8115a8d8993144e75c1fe742ed6c2b1d8a9745b
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2025-01-04 21:47:57 +0900

    IndexTopImgコンポーネントの作成 #19

---
+++ src/components/ui/IndexTopImg.tsx
@@ -0,0 +1,11 @@
+const IndexTopImg = () => {
+    return (
+        <div className=" relative w-full h-[100vh]  bg-[url('/img/top.png')] bg-cover  bg-no-repeat bg-[50%_100%]">
+            <h1 className=" absolute top-[30%] w-full text-[36px] md:text-[52px] lg:text-[64px] text-white  text-center font-normal text-shadow-lg">
+                Nayuta Kumagai<br/>Portfolio
+            </h1>
+        </div>
+    );
+}
+ 
+export default IndexTopImg;
