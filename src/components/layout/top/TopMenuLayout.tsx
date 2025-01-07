85eec2beb1ff16066dd2662677f3239815ce01d0
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2025-01-07 16:23:57 +0900

    目次へ戻るボタンを押したら遷移するようにidを設定 #13

--- src/components/layout/top/TopMenuLayout.tsx
+++ src/components/layout/top/TopMenuLayout.tsx
@@ -4,7 +4,7 @@ import TopMenuItem from "@/components/ui/TopMenuItem";
 const TopMenuLayout = () => {
     return (
         <IndexItemLayout>
-            <div className=" md:pl-[2%] lg:pl-[3%] flex flex-col md:flex-row items-center  gap-10 ss:gap-12  md:gap-[8%] ">
+            <div id="menu" className="pt-2 md:pl-[2%] lg:pl-[3%] flex flex-col md:flex-row items-center  gap-10 ss:gap-12  md:gap-[8%] ">
                 <TopMenuItem link="/#profile" src="/img/profile.png" title="Profile"/>
                 <TopMenuItem link="/#skill" src="/img/skill.png" title="Skill"/>
                 <TopMenuItem link="/#works" src="/img/works.png" title="Works"/>
