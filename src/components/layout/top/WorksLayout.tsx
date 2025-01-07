ebd552acecade7d24b1e0181862abc25329f53c6
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2025-01-07 16:03:20 +0900

    Itemのコンポーネントの修正および WorksLayoutの作成 #14 #23

--- src/components/layout/top/WorksLayout.tsx
+++ src/components/layout/top/WorksLayout.tsx
@@ -1,20 +1,20 @@
 import WorksItem from "@/components/ui/WorksItem";
 import IndexItemLayout from "./IndexItemLayout";
+import H2 from "@/components/common/heading/H2";
 
 
 const WorksLayout = () => {
     return (
-        <div className=" relative sm:pl-[5%] md:pl-[2%] lg:pl-[5%]  bg-[url('/img/works-bg.png')] bg-fixed bg-cover  bg-no-repeat bg-[0%_40%]">
+        <div id="works" className="nav pt-6 md:pt-10 relative bg-[url('/img/works-bg.png')] bg-fixed bg-cover  bg-no-repeat bg-[0%_40%]">
             <IndexItemLayout>
-                <div className="flex flex-wrap  flex-col sm:flex-row gap-[8%]">
-                    <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/a" imgPath="/img/ideus/ideus.png" openFlg />
-                    <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/a" imgPath="/img/ideus/ideus.png" />
-                    <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/a" imgPath="/img/ideus/ideus.png" />
-                    <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/a" imgPath="/img/ideus/ideus.png" />
-                    <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/a" imgPath="/img/ideus/ideus.png" />
-                    <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/a" imgPath="/img/ideus/ideus.png" />
+                <H2>Works</H2>
+                <div className="pt-4 md:mt-5 lg:mt-6 sm:pl-[5%] md:pl-[2%] lg:pl-[3%] flex flex-wrap  flex-col sm:flex-row ss:gap-[8%] md:gap-[5%] lg:gap-[8%]">
+                    <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/ideus" imgPath="/img/ideus/ideus.png" openFlg />
+                    <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/ideus" imgPath="/img/ideus/ideus.png" />
+                    <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/ideus" imgPath="/img/ideus/ideus.png" />
+                    <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/ideus" imgPath="/img/ideus/ideus.png" />
+                    <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/ideus" imgPath="/img/ideus/ideus.png" />
                 </div>
-                <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/a" imgPath="/img/ideus/ideus.png" />
             </IndexItemLayout>
         </div>
     );
--- src/components/ui/WorksItem.tsx
+++ src/components/ui/WorksItem.tsx
@@ -12,14 +12,14 @@ type WorksItemProps = {
  
 const WorksItem: FC<WorksItemProps> = ({title,text,link, imgPath,openFlg=false}) => {
     return (
-        <div className="m-auto sm:m-0 relative w-4/5 pb-9 ss:w-[70%] sm:w-[40%] md:w-[25%]">
+        <div className="m-auto sm:m-0 relative w-[83%] pb-9 ss:w-[75%] sm:w-[40%] md:w-[30%] lg:w-[26%]">
             <h3 className="font-black mb-1 text-[22px] lg:text-[26px] ">{title}</h3>
             <p className="ml-1 mb-2 whitespace-nowrap">{text}</p>
-            <Link href={link} className="flex items-center overflow-hidden  rounded-[20px] shadow-rb w-full max-h-[48vw] ss:h-[40vw]  sm:h-auto  min-h-[180px]  ss:min-h-0   hover:opacity-80 active:opacity-70">
-                <Image className="w-full aspect-square object-cover" src={imgPath} alt={title} width={100} height={100}  unoptimized={true}  quality={100}   priority  />
+            <Link href={link} className="flex items-center overflow-hidden  rounded-[20px] shadow-rb w-full md:w-[90%] lg:w-full  max-h-[48vw] ss:h-[40vw]  sm:h-auto  min-h-[180px]  ss:min-h-0   hover:opacity-80 active:opacity-70">
+                <Image className="w-full  aspect-square object-cover" src={imgPath} alt={title} width={100} height={100}  unoptimized={true}  quality={100}   priority  />
             </Link>
             {openFlg &&         
-                <p className="absolute bottom-2 right-3 text-sm text-[#FA2323] text-right">アプリ公開中</p>
+                <p className="absolute bottom-2 right-3 md:right-6  lg:right-3 text-sm text-[#FA2323] text-right">アプリ公開中</p>
             }
         </div>  
     );
