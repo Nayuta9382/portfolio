dbd76131fe0c7d8916dc9948dc0177e9061d3e03
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2025-01-07 15:56:24 +0900

    ディレクトリ構造を変更 #13

--- src/components/layout/top/TopMenuLayout.tsx
+++ src/components/layout/top/TopMenuLayout.tsx
@@ -1,10 +1,10 @@
 import IndexItemLayout from "@/components/layout/top/IndexItemLayout";
-import TopMenuItem from "./TopMenuItem";
+import TopMenuItem from "@/components/ui/TopMenuItem";
 
-const TopMenu = () => {
+const TopMenuLayout = () => {
     return (
         <IndexItemLayout>
-            <div className="pl-[2%] lg:pl-[5%] flex flex-col md:flex-row items-center  gap-10 md:gap-[8%]">
+            <div className=" md:pl-[2%] lg:pl-[3%] flex flex-col md:flex-row items-center  gap-10 ss:gap-12  md:gap-[8%] ">
                 <TopMenuItem link="/#profile" src="/img/profile.png" title="Profile"/>
                 <TopMenuItem link="/#skill" src="/img/skill.png" title="Skill"/>
                 <TopMenuItem link="/#works" src="/img/works.png" title="Works"/>
@@ -13,4 +13,4 @@ const TopMenu = () => {
     );
 }
  
-export default TopMenu;
+export default TopMenuLayout;
--- src/components/ui/TopMenuItem.tsx
+++ src/components/ui/TopMenuItem.tsx
@@ -11,9 +11,9 @@ type TopMenuItemProps = {
  
 const TopMenuItem: FC<TopMenuItemProps> = ({link,src, alt = '', title}) => {
     return (
-        <Link href={link} className="flex items-center overflow-hidden relative  rounded-[20px] shadow-rb w-[80%] ss:w-[70%] md:w-1/4 max-h-[48vw] ss:h-[40vw]  md:h-auto  min-w-[250px]  sm:min-w-0 min-h-[180px]  ss:min-h-0 hover:opacity-90 active:opacity-80">
+        <Link href={link} className="flex items-center overflow-hidden relative  rounded-[20px] shadow-rb w-[83%] ss:w-[75%] md:w-[27%] lg:w-[26%] max-h-[48vw] ss:h-[40vw]  md:h-auto  min-w-[250px]  sm:min-w-0 min-h-[180px]  ss:min-h-0 hover:opacity-90 active:opacity-80">
             <Image className="w-full h-auto  min-w-[250px] sm:min-w-0 min-h-[250px] sm:min-h-0" src={src} alt={alt} width={100} height={100}  unoptimized={true}  quality={100}   priority  />
-            <p className="absolute bottom-1 lg:bottom-3 md:right-3 right-5 text-2xl lg:text-3xl text-[rgb(255,255,255,0.85)]  font-bold text-right text-shadow-md ">{title}</p>
+            <p className="absolute bottom-1 md:bottom-2 md:right-3 sm:right-5 right-3 text-2xl lg:text-3xl text-[rgb(255,255,255,0.85)]  font-bold text-right text-shadow-md ">{title}</p>
         </Link>
     );
 }
