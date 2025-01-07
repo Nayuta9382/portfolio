abcaa813c6ca90102f420dcaf0010eca4567d0c5
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2025-01-07 16:10:34 +0900

    トップページ(index.tsx)を作成 #22

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,27 @@
-import H2 from "@/components/common/heading/H2";
-import H3 from "@/components/common/heading/H3";
-import List from "@/components/common/List";
-import ListItem from "@/components/common/ListItem";
-import Text from "@/components/common/Text";
-
+import Main from "@/components/layout/Main";
+import ProfileLayout from "@/components/layout/top/ProfileLayout";
+import SkillLayout from "@/components/layout/top/SkillLayout";
+import TopMenuLayout from "@/components/layout/top/TopMenuLayout";
+import WorksLayout from "@/components/layout/top/WorksLayout";
+import IndexTopImg from "@/components/ui/IndexTopImg";
 
 export default function Home() {
+  const navPageData = [{id:'',name:'Top',url:'/'}];
+  const navData = [
+    {id:'profile', name:'Profile',url:'/'},
+    {id:'skill', name: 'Skill',url:'/'},
+    {id:'works', name:'Works',url:'/'} 
+  ];
+ 
   return (  
-    <>
-      <List>
-        <ListItem>熊谷奈友多</ListItem>
-        <ListItem>２文メデス</ListItem>
-        <ListItem>３文メデス</ListItem>
-      </List>
-      <Text lavel="main">メイン</Text>
-      <Text lavel="heading">見出し</Text>
-      <H2>H2です</H2>
-      <H3>H2です</H3>
-     
-    </>
+    <Main navPageData={navPageData} navData={navData}>
+      <IndexTopImg/>
+      <div className="mt-20 flex flex-col gap-16">
+        <TopMenuLayout/>
+        <ProfileLayout/>
+        <SkillLayout/>
+        <WorksLayout/>
+      </div>
+    </Main>
   );
 }
