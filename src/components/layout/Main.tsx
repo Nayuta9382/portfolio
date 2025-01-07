eb6a923f572fce5e43ec13669d3009e14430041b
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2025-01-07 15:56:59 +0900

    mainコンポーネントの構造を変更 #18

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -4,17 +4,20 @@ import Sidebar from "./Sidebar";
 import { navMenu } from "@/types/navMenu";
 
 type MainProps = {
-    children: React.ReactNode;  
-    navData:navMenu[];
+    children: React.ReactNode; 
+    navPageData:navMenu[]; // navに表示するページ情報
+    navData:navMenu[]; // navに表示するHtmlId情報
 }
  
-const Main: FC<MainProps> = ({children,navData}) => {
+const Main: FC<MainProps> = ({children,navPageData,navData}) => {
     return (
-        <main className="relative min-w-[375px] max-w-[1280px]">
+        <>
+        <main className="relative w-full min-w-[355px] max-w-[1280px] mb-4">
             {children}
-            <Sidebar titles={[{id:'',name:'Top',url:'/'},{id:'',name:'Top',url:'/'}]} navInfo={navData} />
-            <Footer/>
+            <Sidebar titles={navPageData} navInfo={navData} />
         </main>
+        <Footer/>
+        </>
     );
 }
  
