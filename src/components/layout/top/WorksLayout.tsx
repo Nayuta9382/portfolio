c2065e3491f9f26934b286394cc94908aa3e3916
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2025-01-14 16:14:53 +0900

    cssの修正 #23

--- src/components/layout/top/WorksLayout.tsx
+++ src/components/layout/top/WorksLayout.tsx
@@ -8,7 +8,7 @@ const WorksLayout = () => {
         <div id="works" className="nav pt-6 md:pt-10 relative bg-[url('/img/works-bg.png')] bg-fixed bg-cover  bg-no-repeat bg-[0%_40%]">
             <IndexItemLayout>
                 <H2>Works</H2>
-                <div className="pt-4 md:mt-5 lg:mt-6 sm:pl-[5%] md:pl-[2%] lg:pl-[3%] flex flex-wrap  flex-col sm:flex-row ss:gap-[8%] md:gap-[5%] lg:gap-[8%]">
+                <div className="pt-4 md:mt-5 lg:mt-2 sm:pl-[5%] md:pl-[2%] lg:pl-[3%] flex flex-wrap  flex-col sm:flex-row ss:gap-[8%] md:gap-[5%] lg:gap-[8%]">
                     <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/ideus" imgPath="/img/ideus/ideus.png" openFlg />
                     <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/ideus" imgPath="/img/ideus/ideus.png" />
                     <WorksItem title="Ideus" text="ひとり暮らし情報共有アプリ" link="/ideus" imgPath="/img/ideus/ideus.png" />
