6e533d4acb1d7885ec421dc64e5e7f5846e261e9
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2025-01-14 17:07:24 +0900

    画像拡大時のコンポーネント作成とdbMenuの画像コンポーネントのリファクタリングの実施 #10 #30

---
+++ src/components/ui/ImageZoom.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { FC } from "react";
+
+type ImageZoomProps = {
+    imgPath:string;
+    // 親から画像の表示・非表示のflgと閉じるonClickの関数を受け取る
+    showImgFlg:boolean;
+    closeImg : () => void;
+}
+
+// 画像を拡大表示するcomponent
+const ImageZoom: FC<ImageZoomProps> = ({imgPath, showImgFlg, closeImg}) => {
+
+    const style  = {
+       display : showImgFlg ? 'block' : 'none'
+      }
+    return (
+        <div style={style} className="bg-[rgb(125,125,125,0.6)]  fixed top-0 left-0  min-w-[99vw] min-h-[100vh] z-50 ">
+            <button onClick={closeImg}  type="button" className="block absolute top-1 right-3 w-7 h-7">
+                <Image className="hover:opacity-80 active:opacity-70" src={'/img/icon/close.png'} alt="" width={28} height={28}/>
+            </button>
+            <Image className="max-w-[90%] max-h-[500px] min-w-[60%] h-auto m-auto mt-10" src={imgPath} alt="" width={100} height={100}   unoptimized={true}  quality={100}/>
+        </div>
+    );
+}
+ 
+export default ImageZoom;
--- src/components/ui/dbMenu/DbShowImg.tsx
+++ src/components/ui/dbMenu/DbShowImg.tsx
@@ -1,6 +1,7 @@
 import useWindowSize from "@/hooks/useWindowSize";
 import Image from "next/image";
 import { FC } from "react";
+import ImageZoom from "../ImageZoom";
 
 interface DbShowImgProps {
     imgPath: string;
@@ -12,26 +13,15 @@ interface DbShowImgProps {
 const DbShowImg: FC<DbShowImgProps> = ({imgPath,upImgFlg,closeImg}) => {
     // 現在の画面幅を取得
   const windowSize = useWindowSize();
-    
-  const imgDivStyle = {
-    display : windowSize < 768 && upImgFlg ? 'block' :'contents' 
-  }
-  
-  const closeButtonStyle = {
-    display: upImgFlg ? 'block' : 'none'
-  }
 
-   // 画面幅が768px以上かでstyleを切り変える
-    const ImgStyle = {
-      display : windowSize >= 768 || upImgFlg ? 'block' : 'none'
-    }
     return (
-        <div style={imgDivStyle} className="bg-[rgb(125,125,125,0.6)]  fixed top-0 left-0  min-w-[99vw] min-h-[100vh] z-50 ">
-            <button style={closeButtonStyle} onClick={closeImg} type="button" className="block absolute top-1 right-3 w-7 h-7">
-                <Image className="hover:opacity-80 active:opacity-70" src={'/img/icon/close.png'} alt="" width={28} height={28}/>
-            </button>
-            <Image style={ImgStyle} className="w-[90%] md:w-full h-auto max-h-[500px] m-auto mt-10" src={imgPath} alt="" width={100} height={100}   unoptimized={true}  quality={100}/>
-      </div>
+        <>
+          {windowSize >= 768 ?(
+            <Image className="w-[90%] md:w-full h-auto max-h-[500px] m-auto mt-10" src={imgPath} alt="" width={100} height={100}   unoptimized={true}  quality={100}/>
+          ):(
+            <ImageZoom imgPath={imgPath} showImgFlg={upImgFlg} closeImg={closeImg}/>
+          )}
+        </>
     );
 }
  
