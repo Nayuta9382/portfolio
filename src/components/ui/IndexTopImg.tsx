75e9133e38f9179aa08571e4f6ac18e01348d8aa
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2025-01-17 11:28:42 +0900

    #19 heightの条件を追加

--- src/components/ui/IndexTopImg.tsx
+++ src/components/ui/IndexTopImg.tsx
@@ -1,6 +1,6 @@
 const IndexTopImg = () => {
     return (
-        <div className="not-show-sidebar relative w-full h-[100vh]  bg-[url('/img/top.png')] bg-cover  bg-no-repeat bg-[50%_100%]">
+        <div className="not-show-sidebar relative w-full h-[100vh] max-h-[800px]  bg-[url('/img/top.png')] bg-cover  bg-no-repeat bg-[50%_100%]">
             <h1 className=" absolute top-[30%] w-full text-[36px] md:text-[52px] lg:text-[64px] text-white  text-center font-normal text-shadow-lg">
                 Nayuta Kumagai<br/>Portfolio
             </h1>
