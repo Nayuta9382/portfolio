d2cefd0e0ec2a36bfa41ed6f6ef6b3fb0b1fa071
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2025-05-27 23:29:52 +0900

    画像サイズを圧縮

--- src/components/ui/TopMenuItem.tsx
+++ src/components/ui/TopMenuItem.tsx
@@ -12,7 +12,7 @@ type TopMenuItemProps = {
 const TopMenuItem: FC<TopMenuItemProps> = ({link,src, alt = '', title}) => {
     return (
         <Link href={link} className="flex items-center overflow-hidden relative  rounded-[20px] shadow-rb w-[83%] ss:w-[75%] md:w-[27%] lg:w-[26%] max-h-[48vw] ss:h-[40vw]  md:h-auto  min-w-[250px]  sm:min-w-0 min-h-[180px]  ss:min-h-0 hover:opacity-90 active:opacity-80">
-            <Image className="w-full h-auto  min-w-[250px] sm:min-w-0 min-h-[250px] sm:min-h-0" src={src} alt={alt} width={100} height={100}   quality={60}   priority  />
+            <Image className="w-full h-auto  min-w-[250px] sm:min-w-0 min-h-[250px] sm:min-h-0" src={src} alt={alt} width={100} height={100}  unoptimized={true}  quality={60}   priority  />
             <p className="absolute bottom-1 md:bottom-2 md:right-3 sm:right-5 right-3 text-2xl lg:text-3xl text-[rgb(255,255,255,0.85)]  font-bold text-right text-shadow-md ">{title}</p>
         </Link>
     );
